86ef9484e34d82831236e952af83e315970037b3
Christian State <junjao.ccomp@gmail.com>
2024-09-30 02:08:21 -0300

    level1 to level2 transition

---
+++ Assets/FinishLevel.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLevel : MonoBehaviour
+{
+    private BoxCollider2D collider = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isTouchingPlayer = collider.IsTouchingLayers(LayerMask.GetMask("Player"));
+
+        if (isTouchingPlayer)
+        {
+            Debug.Log("Player reached the end of the level!");
+            SceneManager.LoadScene("Level2");
+        }
+    }
+}
